51dfbca42c657023faa54b4afcfab9f101133551
Carsten Gräf <evandor@gmail.com>
2016-04-15 10:12:55 +0200

    coverage

---
+++ skysail.server/test/io/skysail/server/security/config/test/AbstractPathToAuthenticatorMatcherTest.java
@@ -0,0 +1,51 @@
+package io.skysail.server.security.config.test;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import io.skysail.api.um.AuthenticationService;
+import io.skysail.server.security.config.AbstractPathToAuthenticatorMatcher;
+import io.skysail.server.security.config.AlwaysAuthenticatedAuthenticator;
+import io.skysail.server.security.config.AuthenticatedAuthenticator;
+import io.skysail.server.security.config.NeverAuthenticatedAuthenticator;
+
+public class AbstractPathToAuthenticatorMatcherTest {
+
+	private AbstractPathToAuthenticatorMatcher matcher;
+
+	@Before
+	public void setUp() {
+		matcher = new AbstractPathToAuthenticatorMatcher() {
+			@Override
+			public boolean match(String path) {
+				return false;
+			}
+		};
+	}
+
+	@Test
+	public void testPermitAll() {
+		matcher.permitAll();
+		assertThat(matcher.getAuthenticator(null, null), instanceOf(AlwaysAuthenticatedAuthenticator.class));
+	}
+
+	@Test
+	public void testAuthenticated() throws Exception {
+		matcher.authenticated();
+		AuthenticationService authService = Mockito.mock(AuthenticationService.class);
+		Mockito.when(authService.getAuthenticator(null)).thenReturn(new AuthenticatedAuthenticator(null));
+		assertThat(matcher.getAuthenticator(null, authService), instanceOf(AuthenticatedAuthenticator.class));
+	}
+
+	@Test
+	public void testDenyAll() throws Exception {
+		matcher.denyAll();
+		assertThat(matcher.getAuthenticator(null, null), instanceOf(NeverAuthenticatedAuthenticator.class));
+	}
+
+
+}
