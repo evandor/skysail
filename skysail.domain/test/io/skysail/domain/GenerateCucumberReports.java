ca832aaaf19d2f47dde4aac67fa53f0185057b37
Carsten Gräf <evandor@gmail.com>
2016-04-27 18:47:11 +0200

    testing cucumber gradle plugin

--- skysail.domain/test/io/skysail/domain/GenerateCucumberReports.java
+++ skysail.domain/test/io/skysail/domain/GenerateCucumberReports.java
@@ -1,30 +1,30 @@
-package io.skysail.domain;
-
-//import net.masterthought.cucumber.Configuration;
-//import net.masterthought.cucumber.ReportBuilder;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-
-public class GenerateCucumberReports {
-
-	public static void main(String[] args) throws Exception {
-		File reportOutputDirectory = new File("generated/cucumber-html-json-reports");
-		List<String> jsonReportFiles = new ArrayList<String>();
-		jsonReportFiles.add("generated/cucumber.json");
-
-		String buildNumber = "1";
-		String buildProjectName = "super_project";
-		Boolean skippedFails = false;
-		Boolean undefinedFails = false;
-//		Boolean flashCharts = true;
-//		Boolean runWithJenkins = false;
-//		Configuration configuration = new Configuration(reportOutputDirectory, "projectName");
-////				 reportOutputDirectory, "", "95",
-////					"cucumber-jvm", false, false, true, true, false, "", true
-//		ReportBuilder reportBuilder = new ReportBuilder(jsonReportFiles,configuration);
-//		reportBuilder.generateReports();
-
-	}
-}
+//package io.skysail.domain;
+//
+////import net.masterthought.cucumber.Configuration;
+////import net.masterthought.cucumber.ReportBuilder;
+//
+//import java.io.File;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class GenerateCucumberReports {
+//
+//	public static void main(String[] args) throws Exception {
+//		File reportOutputDirectory = new File("generated/cucumber-html-json-reports");
+//		List<String> jsonReportFiles = new ArrayList<String>();
+//		jsonReportFiles.add("generated/cucumber.json");
+//
+//		String buildNumber = "1";
+//		String buildProjectName = "super_project";
+//		Boolean skippedFails = false;
+//		Boolean undefinedFails = false;
+////		Boolean flashCharts = true;
+////		Boolean runWithJenkins = false;
+////		Configuration configuration = new Configuration(reportOutputDirectory, "projectName");
+//////				 reportOutputDirectory, "", "95",
+//////					"cucumber-jvm", false, false, true, true, false, "", true
+////		ReportBuilder reportBuilder = new ReportBuilder(jsonReportFiles,configuration);
+////		reportBuilder.generateReports();
+//
+//	}
+//}
