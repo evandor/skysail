8e7585e64c9a0afae882afaf9f991f691358616c
evandor34 <evandor@gmail.com>
2016-05-02 17:12:51 +0200

    wip

--- skysail.server.app.demo.it.httpbasic/src/io/skysail/server/app/demo/it/httpbasic/UnprotectedResourceIntegrationTests.java
+++ skysail.server.app.demo.it.httpbasic/src/io/skysail/server/app/demo/it/httpbasic/UnprotectedResourceIntegrationTests.java
@@ -3,6 +3,7 @@ package io.skysail.server.app.demo.it.httpbasic;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -10,13 +11,17 @@ import org.restlet.data.MediaType;
 import org.restlet.representation.Representation;
 
 import io.skysail.client.testsupport.BrowserTests;
-import io.skysail.client.testsupport.authentication.AuthenticationStrategy;
 import io.skysail.server.app.demo.Time;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class UnprotectedResourceIntegrationTests extends BrowserTests<UnprotectedResourceBrowser, Time> {
 
     @Before
     public void setUp() {
+    	Arrays.stream(thisBundle.getBundleContext().getBundles()).forEach(b -> {
+    		log.info("BUNDLE: " + b.getSymbolicName() + " ["+b.getVersion()+"]");
+    	});
         browser = new UnprotectedResourceBrowser(MediaType.APPLICATION_JSON, determinePort());
     }
 
