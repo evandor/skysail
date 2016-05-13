10c35ac91cb7aa0aff5f6740191e97108f2fa581
Carsten Gräf <evandor@gmail.com>
2016-05-13 11:51:30 +0200

    javadoc fix

--- skysail.server.queryfilter/src/io/skysail/server/queryfilter/parser/Parser.java
+++ skysail.server.queryfilter/src/io/skysail/server/queryfilter/parser/Parser.java
@@ -197,7 +197,7 @@ public class Parser {
             return null;
 
         }
-        case '§': { // "element of", "is in", not standard LDAP syntax! will replace this whole thing with a ANTLR-based grammar
+        case '\u00A7': { // §, "element of", "is in", not standard LDAP syntax! will replace this whole thing with a ANTLR-based grammar
             pos++;
             Object string = parse_substring();
 
@@ -219,7 +219,7 @@ public class Parser {
 
         char c = filterChars[pos];
 
-        while (c != '~' && c != '§' && c != '<' && c != '>' && c != '=' && c != '(' && c != ')') {
+        while (c != '~' && c != '\u00A7' && c != '<' && c != '>' && c != '=' && c != '(' && c != ')') {
             pos++;
 
             if (!Character.isWhitespace(c)) {
