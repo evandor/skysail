af4f23ea487787ba59c537a694b882f60a571714
Carsten Gräf <evandor@gmail.com>
2016-05-14 08:20:35 +0200

    wip

--- skysail.api/src/io/skysail/api/features/package-info.java
+++ skysail.api/src/io/skysail/api/features/package-info.java
@@ -9,7 +9,7 @@
  * <pre>
  *   public enum SecurityFeatures implements Feature {
  *
- *     {@literal @}Label("toggle usage of the cache for user credentials")
+ *     &#64;Label("toggle usage of the cache for user credentials")
  *     USE_CREDENTIALS_CACHE_FEATURE;
  *
  *     public boolean isActive() {
@@ -18,28 +18,28 @@
  *   }
  * </pre>
  *
- * Create an enum implementing {@link Feature} with an "isActive" method like above and use it like this:
+ * Create an enum (implementing Feature) with an "isActive" method like above and use it like this:
  *
- * <pre><code>
- * if (SecurityFeatures.USE_CREDENTIALS_CACHE_FEATURE.isActive()) {
+ * <pre>
+ *   if (SecurityFeatures.USE_CREDENTIALS_CACHE_FEATURE.isActive()) {
  *     ...
- * }
- * </code></pre>
+ *   }
+ * </pre>
  *
  * Now, in the configuration you could define something like this:
  *
- * <pre><code>
- * io.skysail.server.features.SecurityFeatures.USE_CREDENTIALS_CACHE_FEATURE = true
- * </code></pre>
+ * <pre>
+ *   io.skysail.server.features.SecurityFeatures.USE_CREDENTIALS_CACHE_FEATURE = true
+ * </pre>
  *
  * to switch on "USE_CREDENTIALS_CACHE_FEATURE" for all users. Or, you can define
  * some more logic, using a strategy:
  *
- * <pre><code>
- * io.skysail.server.features.SecurityFeatures.USE_CREDENTIALS_CACHE_FEATURE = true
- * io.skysail.server.features.SecurityFeatures.strategy = username
- * io.skysail.server.features.SecurityFeatures.param.users = admin, dev
- * </code></pre>
+ * <pre>
+ *   io.skysail.server.features.SecurityFeatures.USE_CREDENTIALS_CACHE_FEATURE = true
+ *   io.skysail.server.features.SecurityFeatures.strategy = username
+ *   io.skysail.server.features.SecurityFeatures.param.users = admin, dev
+ * </pre>
  *
  * Using different strategies, you can switch on the feature at a given date,
  * restrict it it IP addresses, or apply any custom logic you might need.
