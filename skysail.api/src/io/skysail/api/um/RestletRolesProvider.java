7330159df9a8c09f449262d3d9065f6b8760f10b
Carsten Gräf <evandor@gmail.com>
2016-05-15 15:46:06 +0200

    fix

--- skysail.api/src/io/skysail/api/um/RestletRolesProvider.java
+++ skysail.api/src/io/skysail/api/um/RestletRolesProvider.java
@@ -1,15 +1,16 @@
-//package io.skysail.api.um;
-//
-//import java.util.List;
-//
-//import org.osgi.annotation.versioning.ProviderType;
-//import org.restlet.security.Role;
-//
-//@ProviderType
-//public interface RestletRolesProvider {
-//
-//    Role getRole(String name);
-//
-//    List<Role> getRoles();
-//
-//}
+package io.skysail.api.um;
+
+import java.util.List;
+
+import org.osgi.annotation.versioning.ProviderType;
+import org.restlet.security.Role;
+
+@ProviderType
+// TODO check relevance
+public interface RestletRolesProvider {
+
+    Role getRole(String username);
+
+    List<Role> getRoles();
+
+}
--- skysail.api/src/io/skysail/api/um/UserManagementProvider.java
+++ skysail.api/src/io/skysail/api/um/UserManagementProvider.java
@@ -5,6 +5,9 @@ import org.osgi.annotation.versioning.ProviderType;
 /**
  * Wraps specific authentication and authorization services.
  *
+ * In a skysail installation, there can be only one UserManagementProvider,
+ * providing exactly one authentication and one authorization service.
+ *
  */
 @ProviderType
 public interface UserManagementProvider {
