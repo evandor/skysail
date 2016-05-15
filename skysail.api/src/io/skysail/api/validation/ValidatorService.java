e6814a46ba64d68f3f4ab5c97e1637c89add3254
Carsten Gräf <evandor@gmail.com>
2016-05-15 16:08:31 +0200

    fix

--- skysail.api/src/io/skysail/api/validation/ValidatorService.java
+++ skysail.api/src/io/skysail/api/validation/ValidatorService.java
@@ -5,6 +5,7 @@ import javax.validation.Validator;
 import org.osgi.annotation.versioning.ProviderType;
 
 @ProviderType
+@FunctionalInterface
 public interface ValidatorService {
 
     Validator getValidator();
---
+++ skysail.api/src/io/skysail/api/validation/package-info.java
@@ -0,0 +1,4 @@
+@Version("0.2.2")
+package io.skysail.api.validation;
+
+import org.osgi.annotation.versioning.Version;
