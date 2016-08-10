7c43f04169040379c6cb8d963132afff9b8267ad
Carsten Gräf <evandor@gmail.com>
2016-08-10 18:49:09 +0200

    activating cucumber tests again

--- skysail.server.app.reference.singleentity/test/io/skysail/server/app/reference/singleentity/RunCucumberTests.java
+++ skysail.server.app.reference.singleentity/test/io/skysail/server/app/reference/singleentity/RunCucumberTests.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
-//@RunWith(Cucumber.class)
+@RunWith(Cucumber.class)
 @CucumberOptions(
         //features
         plugin = {"pretty", "json:generated/cucumber.json"}
