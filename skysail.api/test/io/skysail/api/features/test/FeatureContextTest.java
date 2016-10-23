4692d02b9e83d439c9b3a0ae3d52eca4c06afa33
Carsten Gräf <evandor@gmail.com>
2016-10-23 10:06:16 +0200

    wip

--- skysail.api/test/io/skysail/api/features/test/FeatureContextTest.java
+++ skysail.api/test/io/skysail/api/features/test/FeatureContextTest.java
@@ -1,6 +1,7 @@
 package io.skysail.api.features.test;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
 import org.junit.Before;
@@ -15,19 +16,35 @@ import io.skysail.api.features.FeatureStateRepository;
 public class FeatureContextTest {
 
     private FeatureContext featureContext;
+    private FeatureStateRepository repo;
 
     @Before
     public void setup() {
         featureContext = new FeatureContext();
+        repo = Mockito.mock(FeatureStateRepository.class);
+        featureContext.addFeaturesStateRepository(repo);
     }
 
     @Test
     public void featureContext_passes_stateRepository_to_FeatureManager() {
-        FeatureStateRepository repo = Mockito.mock(FeatureStateRepository.class);
-        featureContext.addFeaturesStateRepository(repo);
         FeatureManager featureManager = FeatureContext.getFeatureManager();
         assertThat(featureManager.getClass().getName(),is(DefaultFeatureManager.class.getName()));
         assertThat(featureManager.getStateRepositories().size(),is(1));
         assertThat(featureManager.getStateRepositories().get(0),is(repo));
     }
+
+    @Test
+    public void featureManager_is_the_same_on_second_call() {
+        FeatureManager featureManager = FeatureContext.getFeatureManager();
+        assertThat(FeatureContext.getFeatureManager(),is(featureManager));
+    }
+
+    @Test
+    public void featureContext_passes_stateRepository_to_FeatureManager2() {
+        featureContext.removeFeaturesStateRepository(repo);
+        FeatureManager featureManager = FeatureContext.getFeatureManager();
+        assertThat(featureManager.getClass().getName(),is(DefaultFeatureManager.class.getName()));
+        assertThat(featureManager.getStateRepositories(),is(notNullValue()));
+    }
+
 }
