4005149853743332a7fa8209d527da76f189e86a
Carsten Gräf <evandor@gmail.com>
2017-01-30 09:13:48 +0100

    wip

--- skysail.server.app.ref.fields/src/io/skysail/server/app/ref/fields/PasswordEntity.java
+++ skysail.server.app.ref.fields/src/io/skysail/server/app/ref/fields/PasswordEntity.java
@@ -1,48 +1,48 @@
-//package io.skysail.server.app.ref.fields;
-//
-//import javax.persistence.Id;
-//import javax.validation.constraints.NotNull;
-//import javax.validation.constraints.Size;
-//
-//import io.skysail.domain.Entity;
-//import io.skysail.domain.html.Field;
-//import io.skysail.domain.html.InputType;
-//import io.skysail.server.codegen.annotations.GenerateResources;
-//import io.skysail.server.polymer.elements.PolymerPageContent;
-//import lombok.Getter;
-//import lombok.Setter;
-//import lombok.ToString;
-//
-//@Getter
-//@Setter
-//@ToString
-//@GenerateResources(application="io.skysail.server.app.ref.fields.FieldsDemoApplication")
-//public class PasswordEntity implements Entity {
-//
-//    @Id
-//    private String id;
-//
-//    @Field(inputType = InputType.POLYMER)
-//    private PolymerPageContent content1;
-//
-//    @Field(inputType = InputType.PASSWORD, cssStyle="background-color: #f3f3f3;border:1px solid gray;padding:12px 0px 6px 0px;")
-//    private String apassword;
-//
-//    @Field(inputType = InputType.POLYMER)
-//    private PolymerPageContent content2;
-//
-//    @Field(inputType = InputType.PASSWORD, cssStyle="background-color: #f3f3f3;border:1px solid gray;padding:12px 0px 6px 0px;")
-//    private String astring = "this string is from the Java AnEntity File " + this.getClass().getName();
-//
-//    @Field(inputType = InputType.POLYMER)
-//    private PolymerPageContent content3;
-//
-//    @Field(inputType = InputType.PASSWORD, cssStyle="background-color: #f3f3f3;border:1px solid gray;padding:12px 0px 6px 0px;")
-//    private String withPlaceholder;
-//
-//    @Field
-//    @NotNull
-//    @Size(min=1)
-//    private String required;
-//
-//}
+package io.skysail.server.app.ref.fields;
+
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import io.skysail.domain.Entity;
+import io.skysail.domain.html.Field;
+import io.skysail.domain.html.InputType;
+import io.skysail.server.codegen.annotations.GenerateResources;
+import io.skysail.server.polymer.elements.PolymerPageContent;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@GenerateResources(application="io.skysail.server.app.ref.fields.PasswordEntityApplication")
+public class PasswordEntity implements Entity {
+
+    @Id
+    private String id;
+
+    @Field(inputType = InputType.POLYMER)
+    private PolymerPageContent content1;
+
+    @Field(inputType = InputType.PASSWORD, cssStyle="background-color: #f3f3f3;border:1px solid gray;padding:12px 0px 6px 0px;")
+    private String apassword;
+
+    @Field(inputType = InputType.POLYMER)
+    private PolymerPageContent content2;
+
+    @Field(inputType = InputType.PASSWORD, cssStyle="background-color: #f3f3f3;border:1px solid gray;padding:12px 0px 6px 0px;")
+    private String astring = "this string is from the Java AnEntity File " + this.getClass().getName();
+
+    @Field(inputType = InputType.POLYMER)
+    private PolymerPageContent content3;
+
+    @Field(inputType = InputType.PASSWORD, cssStyle="background-color: #f3f3f3;border:1px solid gray;padding:12px 0px 6px 0px;")
+    private String withPlaceholder;
+
+    @Field
+    @NotNull
+    @Size(min=1)
+    private String required;
+
+}
---
+++ skysail.server.app.ref.fields/src/io/skysail/server/app/ref/fields/PasswordEntityApplication.java
@@ -0,0 +1,62 @@
+package io.skysail.server.app.ref.fields;
+
+import java.util.Arrays;
+
+import org.osgi.service.cm.ConfigurationException;
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
+import org.osgi.service.component.annotations.Reference;
+
+import io.skysail.server.app.ApiVersion;
+import io.skysail.server.app.ApplicationConfiguration;
+import io.skysail.server.app.ApplicationProvider;
+import io.skysail.server.app.SkysailApplication;
+import io.skysail.server.app.ref.fields.repositories.Repository;
+import io.skysail.server.db.DbService;
+import io.skysail.server.menus.MenuItemProvider;
+import lombok.Getter;
+
+@Component(immediate = true, configurationPolicy = ConfigurationPolicy.OPTIONAL)
+public class PasswordEntityApplication extends SkysailApplication implements ApplicationProvider, MenuItemProvider {
+
+    public static final String APP_NAME = "fieldsdemo";
+
+    @Reference
+    private DbService dbService;
+
+//    @Getter
+//	private EntityWithoutTabssRepo entityWithoutTabssRepo;
+//
+//    @Getter
+//    private TrixEditorEntitysRepo trixEditorEntitysRepo;
+//
+//    @Getter
+//    private NestedEntitysRepo nestedEntitysRepo;
+
+    @Getter
+    private Repository repository;
+
+//    @Getter
+//    private PasswordEntitysRepo passwordEntitysRepo;
+
+    public PasswordEntityApplication() {
+        super(APP_NAME, new ApiVersion(1), Arrays.asList(
+                TextEntity.class));//, PasswordEntity.class, EntityWithoutTabs.class, NestedEntity.class, TrixEditorEntity.class));
+        setDescription("a skysail application");
+    }
+
+    @Activate
+    @Override
+    public void activate(ApplicationConfiguration appConfig, ComponentContext componentContext)
+            throws ConfigurationException {
+        super.activate(appConfig, componentContext);
+//        this.entityWithoutTabssRepo = new EntityWithoutTabssRepo(dbService);
+//        this.trixEditorEntitysRepo = new TrixEditorEntitysRepo(dbService);
+//        this.nestedEntitysRepo = new NestedEntitysRepo(dbService);
+        this.repository = new Repository(dbService);
+//        this.passwordEntitysRepo = new PasswordEntitysRepo(dbService);
+    }
+
+}
