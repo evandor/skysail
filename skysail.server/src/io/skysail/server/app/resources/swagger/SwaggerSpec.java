a646cf37a575934ef40df7f85fe976e4a621c8fc
Carsten Gräf <evandor@gmail.com>
2017-02-06 18:18:17 +0100

    wip

--- skysail.server/src/io/skysail/server/app/resources/swagger/SwaggerSpec.java
+++ skysail.server/src/io/skysail/server/app/resources/swagger/SwaggerSpec.java
@@ -1,5 +1,7 @@
 package io.skysail.server.app.resources.swagger;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -28,25 +30,25 @@ public class SwaggerSpec implements Entity {
 
 	@JsonIgnore
 	private String id;
-	
+
 	private String swagger = "2.0";
-	
+
 	private SwaggerInfo info;
 
 	private String host = "localhost:2021";//: petstore.swagger.io
 	private final String basePath;//: /api
 	private List<String> schemes = Arrays.asList("http");
-		
+
 	private Map<String, SwaggerPath> paths = new HashMap<>();
-	
+
 	private Map<String, SwaggerDefinition> definitions = new HashMap<>();
 
 	@JsonIgnore
     private Set<Class<?>> types = new HashSet<>();
-	
+
 	public SwaggerSpec(SkysailApplication skysailApplication, Request request) {
 		this.info = new SwaggerInfo(skysailApplication);
-		this.host = request.getHostRef().getHostDomain() + ":" + request.getHostRef().getHostPort();
+		this.host = determineHost(request);// request.getHostRef().getHostDomain() + ":" + request.getHostRef().getHostPort();
 		this.basePath = "/" + skysailApplication.getName() + skysailApplication.getApiVersion().getVersionPath();
 		determinePaths(skysailApplication);
 		determineDefinitions(skysailApplication);
@@ -70,9 +72,9 @@ public class SwaggerSpec implements Entity {
 			    }
                 paths.put(path.substring(versionPath.length()), new SwaggerPath(routeBuilder));
 			});
-		
+
 	}
-    
+
     private void determineDefinitions(SkysailApplication skysailApplication) {
         types.stream()
             .sorted((e1,e2) -> e1.getName().compareTo(e2.getName()))
@@ -81,6 +83,18 @@ public class SwaggerSpec implements Entity {
             });
     }
 
+    private String determineHost(Request request) {
+        String hostDomainAsIp = request.getHostRef().getHostDomain();// + ":" + request.getHostRef().getHostPort();
+        try {
+            InetAddress addr = InetAddress.getByName(hostDomainAsIp);
+            String host = addr.getHostName();
+            return host + ":" + request.getHostRef().getHostPort();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+            return request.getHostRef().getHostDomain() + ":" + request.getHostRef().getHostPort();
+        }
+    }
 
-	
+
+
 }
