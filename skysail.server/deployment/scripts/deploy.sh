2dc6f2b823f4142fc329e1140142adab003eea94
evandor <evandor@gmail.com>
2017-02-08 11:11:41 +0100

    pathfix

--- skysail.server/deployment/scripts/deploy.sh
+++ skysail.server/deployment/scripts/deploy.sh
@@ -49,8 +49,8 @@ echo "-------------------"
 
 echo "copying skysail.$APPNAME.zip to public site"
 mkdir -p /var/www/skysail.io/public_html/skysail/products/$APPNAME
-cp skysail.$APPNAME.zip /var/www/skysail.io/public_html/skysail/products/$APPNAME/skysail.$APPNAME.$STAGE.zip
-cp skysail.$APPNAME.jar /var/www/skysail.io/public_html/skysail/products/$APPNAME/skysail.$APPNAME.$STAGE.jar
+cp skysail.$APPNAME.zip /var/www/downloads.skysail.io/skysail/products/$APPNAME/skysail.$APPNAME.$STAGE.zip
+cp skysail.$APPNAME.jar /var/www/downloads.skysail.io/skysail/products/$APPNAME/skysail.$APPNAME.$STAGE.jar
 
 ### STOPPING SERVICE #####################################################
 echo ""
