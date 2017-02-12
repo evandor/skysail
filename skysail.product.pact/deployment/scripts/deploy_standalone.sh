c94f795ef10ec734dd78ad90a51e9c7658798bc0
Carsten Graef <evandor@gmail.com>
2017-02-12 09:42:33 +0100

    wip

---
+++ skysail.product.pact/deployment/scripts/deploy_standalone.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+
+##########################################################################
+### Deployment Script SSP Pact Integration  ##############################
+##########################################################################
+
+CURRENT_DIR=`pwd`
+
+### CONFIGURATION ########################################################
+echo ""
+echo "Script Configuration:"
+echo "---------------------"
+echo ""
+echo "PWD: $CURRENT_DIR"
+echo ""
+
+APPNAME="pact"
+STAGE="standalone"
+
+MAIN_DEPLOY_SCRIPT_PATH=../../../skysail.server/deployment/scripts
+
+cd $CURRENT_DIR
+echo "calling $MAIN_DEPLOY_SCRIPT_PATH/deploy.sh"
+chmod 775 $MAIN_DEPLOY_SCRIPT_PATH/deploy.sh
+source $MAIN_DEPLOY_SCRIPT_PATH/deploy.sh
