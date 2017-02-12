f03aa1ebd4f5752a464d4f695cd95c43eeb7dfee
Carsten Graef <evandor@gmail.com>
2017-02-12 09:33:51 +0100

    wip

--- skysail.product.demo/deployment/scripts/deploy_int.sh
+++ skysail.product.demo/deployment/scripts/deploy_int.sh
@@ -1,20 +1,30 @@
 #!/bin/bash -e
 
 ##########################################################################
-### Deployment Script SSP Demo Integration  ##############################
+### Deployment Script SSP Pact Integration  ##############################
 ##########################################################################
+
+CURRENT_DIR=`pwd`
 
 ### CONFIGURATION ########################################################
 echo ""
 echo "Script Configuration:"
 echo "---------------------"
+echo ""
+echo "PWD: $CURRENT_DIR"
+echo ""
 
 APPNAME="demo"
 STAGE="int"
 
-MAIN_DEPLOY_SCRIPT=../../../skysail.server/deployment/scripts/deploy.sh
-echo "calling $MAIN_DEPLOY_SCRIPT"
+MAIN_DEPLOY_SCRIPT_PATH=../../../skysail.server/deployment/scripts
 
-chmod 775 $MAIN_DEPLOY_SCRIPT
-source $MAIN_DEPLOY_SCRIPT
+cd $CURRENT_DIR
+echo "calling $MAIN_DEPLOY_SCRIPT_PATH/deploy.sh"
+chmod 775 $MAIN_DEPLOY_SCRIPT_PATH/deploy.sh
+source $MAIN_DEPLOY_SCRIPT_PATH/deploy.sh
 
+cd $CURRENT_DIR
+echo "calling $MAIN_DEPLOY_SCRIPT_PATH/startService.sh"
+chmod 775 $MAIN_DEPLOY_SCRIPT_PATH/startService.sh
+source $MAIN_DEPLOY_SCRIPT_PATH/startService.sh
