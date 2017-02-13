15be27c1c9a6fa524c3102ca5102dd1a6862963b
Carsten Graef <carsten@Carstens-MBP.fritz.box>
2017-02-13 19:11:15 +0100

    function in bash script

--- skysail.product.pact/deployment/scripts/deploy_int.sh
+++ skysail.product.pact/deployment/scripts/deploy_int.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 ##########################################################################
-### Deployment Script SSP Pact Integration  ##############################
+### Deployment Script SSP Pact Integration                             ###
 ##########################################################################
 
 CURRENT_DIR=`pwd`
@@ -16,15 +16,25 @@ echo ""
 
 APPNAME="pact"
 STAGE="int"
-
 MAIN_DEPLOY_SCRIPT_PATH=../../../skysail.server/deployment/scripts
 
-cd $CURRENT_DIR
-echo "calling $MAIN_DEPLOY_SCRIPT_PATH/deploy.sh"
-chmod 775 $MAIN_DEPLOY_SCRIPT_PATH/deploy.sh
-source $MAIN_DEPLOY_SCRIPT_PATH/deploy.sh
+function execute {
+  echo "executing $1" 
+  cd $CURRENT_DIR
+  echo "calling $MAIN_DEPLOY_SCRIPT_PATH/$1"
+  chmod 775 $MAIN_DEPLOY_SCRIPT_PATH/$1
+  source $MAIN_DEPLOY_SCRIPT_PATH/$1
+} 
 
-cd $CURRENT_DIR
-echo "calling $MAIN_DEPLOY_SCRIPT_PATH/startService.sh"
-chmod 775 $MAIN_DEPLOY_SCRIPT_PATH/startService.sh
-source $MAIN_DEPLOY_SCRIPT_PATH/startService.sh
+execute deploy.sh
+execute startService.sh
+
+#cd $CURRENT_DIR
+#echo "calling $MAIN_DEPLOY_SCRIPT_PATH/deploy.sh"
+#chmod 775 $MAIN_DEPLOY_SCRIPT_PATH/deploy.sh
+#source $MAIN_DEPLOY_SCRIPT_PATH/deploy.sh
+
+#cd $CURRENT_DIR
+#echo "calling $MAIN_DEPLOY_SCRIPT_PATH/startService.sh"
+#chmod 775 $MAIN_DEPLOY_SCRIPT_PATH/startService.sh
+#source $MAIN_DEPLOY_SCRIPT_PATH/startService.sh
