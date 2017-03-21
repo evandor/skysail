151e25abbe7efae50c7ed45fed597a26e67d66d5
Carsten Gräf <evandor@gmail.com>
2017-03-21 08:51:34 +0100

    wip

---
+++ skysail.app.notes/deployment/scripts/deploy_int.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -e
+
+##########################################################################
+### Deployment Script SSP Pact Integration                             ###
+##########################################################################
+
+CURRENT_DIR=`pwd`
+
+### CONFIGURATION ########################################################
+echo ""
+echo "Script Configuration:"
+echo "---------------------"
+echo ""
+echo "PWD: $CURRENT_DIR"
+echo ""
+
+PROJECTNAME="skysail.app.notes"
+APPNAME="notes"
+STAGE="int"
+MAIN_DEPLOY_SCRIPT_PATH=../../../skysail.server/deployment/scripts
+
+function execute {
+  echo "executing $1" 
+  cd $CURRENT_DIR
+  echo "calling $MAIN_DEPLOY_SCRIPT_PATH/$1"
+  chmod 775 $MAIN_DEPLOY_SCRIPT_PATH/$1
+  source $MAIN_DEPLOY_SCRIPT_PATH/$1
+} 
+
+execute deploy.sh
+execute startService.sh
+
