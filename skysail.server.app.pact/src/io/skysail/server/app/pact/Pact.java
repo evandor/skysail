d34babef341a3be292db79ff40663d36efa5b5f7
Carsten Gräf <evandor@gmail.com>
2017-05-19 06:47:02 +0200

    wip

--- skysail.server.app.pact/src/io/skysail/server/app/pact/Pact.java
+++ skysail.server.app.pact/src/io/skysail/server/app/pact/Pact.java
@@ -13,9 +13,6 @@ public class Pact implements Entity{
     @Field(description = "the pacts title, a descriptive string, not necessarily unique", cssStyle = "background-color:#f9f9f9;padding-top:15px;border:1px solid gray;")
     private String title;
 
-//    @Field(inputType = InputType.READONLY, description = "a list of participant names, fixed for now")
-//    private List<String> members = Arrays.asList("Carsten", "Georgios");
-
     @Field(inputType = InputType.READONLY, description = "a string identifying a selection strategy, fixed for now")
     private String selectionStrategy = "Fixed Order, starting with Georgios";
 
